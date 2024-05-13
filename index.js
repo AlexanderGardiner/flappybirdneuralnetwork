b088c6e7dc6599bc61174562ac8b2912c84ea1f7
AlexanderGardiner <misc@alexandergardiner.com>
2024-05-12 21:07:22 -0700

    ahhh

--- index.js
+++ index.js
@@ -1,40 +1,143 @@
 let sceneCanvas;
 let sceneCanvasCTX;
 let walls;
-let player;
-let gameOver = false;
+let players = [];
+let gameOver = [];
+let networks = [];
+let networksFailureTimes = [];
+let totalAgents = 1;
+let geneticDivergenceFactor = 1;
+
+let mainLoopInterval;
 
 function initializeScene() {
   sceneCanvas = setupCanvas(1920, 1080);
   sceneCanvasCTX = sceneCanvas.getContext("2d");
 
   walls = initializeWalls(5, 100, 200, 300, 800, 500, 700);
-  player = initializePlayer(100, 500, 50, 0.5);
 
-  setInterval(() => {
+  for (let i = 0; i < totalAgents; i++) {
+    players.push(initializePlayer(100, 500, 50, 0.5));
+    networks.push(new NeuralNetwork(4, [4], [3], 1));
+  }
+
+  mainLoopInterval = setInterval(() => {
     mainLoop();
   }, 20);
 }
 
-function mainLoop() {
-  if (!gameOver) {
-    updateWalls(walls, 100, 200, 300, 800, 500);
-    moveWalls(walls);
+function newGeneration(winningNetwork) {
+  console.log(winningNetwork);
+  players = [];
+  networks = [];
+  walls = [];
+  networksFailureTimes = [];
+  gameOver = [];
+  walls = initializeWalls(5, 100, 200, 300, 800, 500, 700);
+  for (let i = 0; i < totalAgents; i++) {
+    players.push(initializePlayer(100, 500, 50, 0.5));
+    networks.push(
+      new NeuralNetwork(
+        4,
+        [4],
+        [3],
+        1,
+        winningNetwork.inputLayer,
+        winningNetwork.hiddenLayers,
+        winningNetwork.outputLayer
+      )
+    );
+    console.log(networks[i]);
+    networks[i].geneticEvolution(geneticDivergenceFactor);
+    console.log(networks[i]);
+  }
 
-    movePlayer(player);
-    updatePlayerVelocity(player);
+  // mainLoopInterval = setInterval(() => {
+  //   mainLoop();
+  // }, 20);
+}
 
+function getAgentAction(network, player, walls) {
+  let closestWall = walls[walls.length - 1];
+  for (let wall of walls) {
     if (
-      detectPlayerWallCollision(player, walls, sceneCanvas.height) ||
-      detectPlayerFloorCollision(player)
+      !(wall.xPosition + wall.width < player.xPosition) &&
+      wall.xPosition < closestWall.xPosition
     ) {
-      gameOver = true;
+      closestWall = wall;
+    }
+  }
+  // console.log(network);
+  let computation = network.computeOutputs([
+    [horizontalDistanceFromPlayerToWall(player, closestWall)],
+    [playerDistanceFromGround(player)],
+    [verticalDistanceFromPlayerToHole(player, closestWall)],
+    [player.velocity],
+  ]);
+
+  return computation > 0.5;
+}
+
+function getMostRecentIndices(array, count) {
+  // Create an array to store the indices of the most recent dates
+  let recentIndices = [];
+
+  // Create a copy of the array to avoid modifying the original array
+  let copyArray = array.slice();
+
+  // Sort the copied array in descending order (most recent dates first)
+  copyArray.sort((a, b) => b - a);
+
+  // Loop through the sorted array and store the indices of the most recent dates
+  for (let i = 0; i < count; i++) {
+    let index = array.indexOf(copyArray[i]);
+    if (index !== -1) {
+      recentIndices.push(index);
     }
   }
 
+  return recentIndices;
+}
+
+function mainLoop() {
   clearCanvas(sceneCanvas, sceneCanvasCTX);
+
+  updateWalls(walls, 100, 200, 300, 800, 500);
+  moveWalls(walls);
+
+  let livingPlayersCount = 0;
+  for (let i = 0; i < players.length; i++) {
+    if (!gameOver[i]) {
+      if (getAgentAction(networks[i], players[i], walls)) {
+        jumpPlayer(players[i], 15);
+      }
+      movePlayer(players[i]);
+      updatePlayerVelocity(players[i]);
+      if (players[i].yPosition + players[i].length > sceneCanvas.height) {
+        players[i].yPosition = sceneCanvas.height - players[i].length - 2;
+      }
+
+      if (
+        detectPlayerWallCollision(players[i], walls, sceneCanvas.height) ||
+        detectPlayerFloorCollision(players[i])
+      ) {
+        gameOver[i] = true;
+        networksFailureTimes[i] = new Date();
+      }
+      drawPlayer(players[i], sceneCanvasCTX, sceneCanvas.height, "blue", 1);
+      livingPlayersCount += 1;
+    } else {
+      drawPlayer(players[i], sceneCanvasCTX, sceneCanvas.height, "red", 0.2);
+    }
+  }
+
   drawWalls(walls, sceneCanvasCTX, sceneCanvas.height);
-  drawPlayer(player, sceneCanvasCTX, sceneCanvas.height);
+
+  if (livingPlayersCount == 0) {
+    clearInterval(mainLoopInterval);
+    console.log(getMostRecentIndices(networksFailureTimes, 1));
+    newGeneration(networks[getMostRecentIndices(networksFailureTimes, 1)]);
+  }
 }
 
 function clearCanvas(sceneCanvas, sceneCanvasCTX) {
@@ -70,8 +173,8 @@ function initializePlayer(xPosition, startingYPosition, length, gravity) {
   return new Player(xPosition, startingYPosition, length, gravity);
 }
 
-function drawPlayer(player, canvasCTX, canvasHeight) {
-  player.draw(canvasCTX, canvasHeight);
+function drawPlayer(player, canvasCTX, canvasHeight, color, alpha) {
+  player.draw(canvasCTX, canvasHeight, color, alpha);
 }
 
 function drawWalls(walls, canvasCTX, canvasHeight) {
@@ -87,8 +190,8 @@ function movePlayer(player) {
   player.yPosition += player.velocity;
 }
 
-function jumpPlayer(player) {
-  player.velocity = 10;
+function jumpPlayer(player, velocity) {
+  player.velocity = velocity;
 }
 
 function moveWalls(walls) {
@@ -173,6 +276,18 @@ function setupCanvas(canvasWidth, canvasHeight) {
   return sceneCanvas;
 }
 
+function horizontalDistanceFromPlayerToWall(player, wall) {
+  return wall.xPosition - (player.xPosition + player.length);
+}
+
+function playerDistanceFromGround(player) {
+  return player.yPosition;
+}
+
+function verticalDistanceFromPlayerToHole(player, wall) {
+  return wall.gapYPosition - player.yPosition;
+}
+
 class Wall {
   constructor(width, xPosition, gapHeight, gapYPosition) {
     this.width = width;
@@ -208,54 +323,136 @@ class Player {
     this.velocity = 0;
   }
 
-  draw(canvasCTX, canvasHeight) {
-    canvasCTX.fillStyle = "blue  ";
+  draw(canvasCTX, canvasHeight, color, alpha) {
+    canvasCTX.fillStyle = color;
+    canvasCTX.globalAlpha = alpha;
     canvasCTX.fillRect(
       this.xPosition,
       canvasHeight - (this.yPosition + this.length),
       this.length,
       this.length
     );
+    canvasCTX.globalAlpha = 1;
   }
 }
 
 class NeuralNetwork {
-  constructor(numberOfInputs, hiddenLayersInputs, numberOfOutputs) {
-    let inputNeurons = [];
-    for (let i = 0; i < numberOfInputs; i++) {
-      inputNeurons.push(new Neuron(1));
-    }
-    this.inputLayer = new Layer(inputNeurons);
-
-    this.hiddenLayers = [];
-    for (let i = 0; i < hiddenLayersInputs.length; i++) {
-      let hiddenLayerNeurons = [];
-      for (let j = 0; j < hiddenLayersInputs[i]; j++) {
-        if (i == 0) {
-          hiddenLayerNeurons.push(new Neuron(numberOfInputs));
-        } else {
-          hiddenLayerNeurons.push(new Neuron(hiddenLayersInputs[i - 1]));
-        }
+  constructor(
+    numberOfInputs,
+    hiddenLayersInputs,
+    hiddenLayersHeights,
+    numberOfOutputs,
+    inputLayer,
+    hiddenLayers,
+    outputLayer
+  ) {
+    if (inputLayer == null) {
+      let inputNeurons = [];
+      for (let i = 0; i < numberOfInputs; i++) {
+        inputNeurons.push(new Neuron(1));
       }
-      this.hiddenLayers.push(new Layer(hiddenLayerNeurons));
+      this.inputLayer = new Layer(inputNeurons);
+    } else {
+      let inputNeurons = [];
+      for (let i = 0; i < numberOfInputs; i++) {
+        inputNeurons.push(
+          new Neuron(
+            1,
+            inputLayer.neurons[i].weights,
+            inputLayer.neurons[i].bias
+          )
+        );
+      }
+      this.inputLayer = new Layer(inputNeurons);
     }
 
-    let outputNeurons = [];
-    for (let i = 0; i < numberOfOutputs; i++) {
-      outputNeurons.push(new Neuron(1));
+    if (hiddenLayers == null) {
+      this.hiddenLayers = [];
+      if (hiddenLayersInputs.length != hiddenLayersHeights.length) {
+        return "There must be an equal number of hidden layers passed for inputs and heights";
+      }
+      for (let i = 0; i < hiddenLayersInputs.length; i++) {
+        let hiddenLayerNeurons = [];
+        for (let j = 0; j < hiddenLayersHeights[i]; j++) {
+          hiddenLayerNeurons.push(new Neuron(hiddenLayersInputs[i]));
+        }
+        this.hiddenLayers.push(new Layer(hiddenLayerNeurons));
+      }
+    } else {
+      this.hiddenLayers = [];
+      if (hiddenLayersInputs.length != hiddenLayersHeights.length) {
+        return "There must be an equal number of hidden layers passed for inputs and heights";
+      }
+      for (let i = 0; i < hiddenLayersInputs.length; i++) {
+        let hiddenLayerNeurons = [];
+        for (let j = 0; j < hiddenLayersHeights[i]; j++) {
+          hiddenLayerNeurons.push(
+            new Neuron(
+              hiddenLayersInputs[i],
+              hiddenLayers[i].neurons[j].weights,
+              hiddenLayers[i].neurons[j].bias
+            )
+          );
+        }
+        this.hiddenLayers.push(new Layer(hiddenLayerNeurons));
+      }
     }
-    this.outputLayer = new Layer(outputNeurons);
+
+    if (outputLayer == null) {
+      let outputNeurons = [];
+      for (let i = 0; i < numberOfOutputs; i++) {
+        outputNeurons.push(
+          new Neuron(
+            this.hiddenLayers[this.hiddenLayers.length - 1].neurons.length
+          )
+        );
+      }
+      this.outputLayer = new Layer(outputNeurons);
+    } else {
+      let outputNeurons = [];
+      for (let i = 0; i < numberOfOutputs; i++) {
+        outputNeurons.push(
+          new Neuron(
+            this.hiddenLayers[this.hiddenLayers.length - 1].neurons.length,
+            outputLayer.weights,
+            outputLayer.bias
+          )
+        );
+      }
+      this.outputLayer = new Layer(outputNeurons);
+    }
+  }
+
+  geneticEvolution(geneticDivergenceFactor) {
+    this.inputLayer.geneticEvolution(geneticDivergenceFactor);
+    for (let i = 0; i < this.hiddenLayers.length; i++) {
+      this.hiddenLayers[i].geneticEvolution(geneticDivergenceFactor);
+    }
+    this.outputLayer.geneticEvolution(geneticDivergenceFactor);
   }
 
   computeOutputs(inputs) {
+    // console.log(inputs);
     let inputLayerOutputs = this.inputLayer.computeOutputs(inputs);
-    let hiddenLayerOutputs = inputLayerOutputs;
+    // console.log("input layer", inputLayerOutputs);
+    let hiddenLayerOutputs = [];
+
+    for (let i = 0; i < this.hiddenLayers[0].neurons.length; i++) {
+      hiddenLayerOutputs.push(inputLayerOutputs);
+    }
+    // console.log("hidden layer inputs", hiddenLayerOutputs);
     for (let i = 0; i < this.hiddenLayers.length; i++) {
       hiddenLayerOutputs =
         this.hiddenLayers[i].computeOutputs(hiddenLayerOutputs);
+      // console.log("hidden layer outputs", hiddenLayerOutputs);
+
+      // console.log("test");
+      // console.log("hidden layer ", i, hiddenLayerOutputs);
     }
-    let outputLayerOutputs =
-      this.outputLayer.computeOutputs(hiddenLayerOutputs);
+
+    let outputLayerOutputs = this.outputLayer.computeOutputs([
+      hiddenLayerOutputs,
+    ]);
 
     return outputLayerOutputs;
   }
@@ -266,28 +463,55 @@ class Layer {
     this.neurons = neurons;
   }
 
+  geneticEvolution(geneticDivergenceFactor) {
+    for (let i = 0; i < this.neurons.length; i++) {
+      console.log("test");
+      console.log(this.neurons[i]);
+      this.neurons[i].geneticEvolution(geneticDivergenceFactor);
+      console.log(this.neurons[i]);
+    }
+  }
+
   computeOutputs(inputs) {
     let outputs = [];
     for (let i = 0; i < this.neurons.length; i++) {
-      for (let j = 0; j < this.neurons[i].weights.length; j++) {
-        outputs.push(this.neurons[i].compute([inputs[j]]));
-      }
+      outputs.push(this.neurons[i].compute(inputs[i]));
     }
     return outputs;
   }
 }
 
 class Neuron {
-  constructor(numberOfInputs) {
-    this.weights = [];
-    this.bias = Math.random() - 0.5;
-    for (let i = 0; i < numberOfInputs; i++) {
-      this.weights.push(Math.random() - 0.5);
+  constructor(numberOfInputs, weights, bias) {
+    if (weights == null) {
+      this.weights = [];
+      for (let i = 0; i < numberOfInputs; i++) {
+        this.weights.push(Math.random() - 0.5);
+      }
+    } else {
+      this.weights = weights;
+    }
+
+    if (this.bias == null) {
+      this.bias = Math.random() - 0.5;
+    } else {
+      this.bias = bias;
     }
   }
 
+  geneticEvolution(geneticDivergenceFactor) {
+    for (let i = 0; i < this.weights.length; i++) {
+      this.weights[i] += (Math.random() - 0.5) * geneticDivergenceFactor;
+    }
+
+    this.bias += (Math.random() - 0.5) * geneticDivergenceFactor;
+  }
+
   compute(inputs) {
+    // console.log(inputs.length);
+    // console.log(this.weights.length);
     if (inputs.length != this.weights.length) {
+      console.log("Error, weights and inputs must have the same length");
       return "Error, weights and inputs must have the same length";
     }
 
@@ -296,13 +520,11 @@ class Neuron {
       output += inputs[i] * this.weights[i];
     }
 
-    output += this.bias;
+    //output += this.bias;
 
     return sigmoid(output);
   }
 }
-let network = new NeuralNetwork(5, [], 1);
-console.log(network.computeOutputs([0, 1, 1, 1, 1]));
 
 function sigmoid(z) {
   return 1 / (1 + Math.exp(-z));
@@ -310,7 +532,9 @@ function sigmoid(z) {
 
 document.addEventListener("keypress", function onEvent(event) {
   if (event.key === " ") {
-    jumpPlayer(player);
+    for (let player of players) {
+      jumpPlayer(player, Math.random() * 10);
+    }
   }
 });
 initializeScene();
